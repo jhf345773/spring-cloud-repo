6bc9e20fb7213eee54698c92ba8a6ae4eb7f248f
jhf345773 <54342824+jhf345773@users.noreply.github.com>
2019-11-01 16:00:13 +0800

    Update FeignController.java

--- sp09-feign/src/main/java/com/tedu/sp09/controller/FeignController.java
+++ sp09-feign/src/main/java/com/tedu/sp09/controller/FeignController.java
@@ -20,43 +20,43 @@ import com.tedu.web.util.JsonResult;
 @RestController
 public class FeignController {
 	@Autowired
-	private ItemFeignService itemServcie;
+	private ItemFeignService itemService;
 	@Autowired
-	private UserFeignService userServcie;
+	private UserFeignService userService;
 	@Autowired
-	private OrderFeignService orderServcie;
+	private OrderFeignService orderService;
 	
 	@GetMapping("/item-service/{orderId}")
 	public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
-		return itemServcie.getItems(orderId);
+		return itemService.getItems(orderId);
 	}
 
 	@PostMapping("/item-service/decreaseNumber")
 	public JsonResult decreaseNumber(@RequestBody List<Item> items) {
-		return itemServcie.decreaseNumber(items);
+		return itemService.decreaseNumber(items);
 	}
 
 	/////////////////////////////////////////
 	
 	@GetMapping("/user-service/{userId}")
 	public JsonResult<User> getUser(@PathVariable Integer userId) {
-		return userServcie.getUser(userId);
+		return userService.getUser(userId);
 	}
 
 	@GetMapping("/user-service/{userId}/score") 
 	public JsonResult addScore(@PathVariable Integer userId, Integer score) {
-		return userServcie.addScore(userId, score);
+		return userService.addScore(userId, score);
 	}
 	
 	/////////////////////////////////////////
 	
 	@GetMapping("/order-service/{orderId}")
 	public JsonResult<Order> getOrder(@PathVariable String orderId) {
-		return orderServcie.getOrder(orderId);
+		return orderService.getOrder(orderId);
 	}
 
 	@GetMapping("/order-service")
 	public JsonResult addOrder() {
-		return orderServcie.addOrder();
+		return orderService.addOrder();
 	}
 }
